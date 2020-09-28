0d003a1d584f2fbad57c3d025e22dd47eaf408cd
torstbje <torstbje@student.matnat.uio.no>
2020-09-29 00:31:28 +0200

    overloaded function input

--- Project2/EigenValueSolver.cpp
+++ Project2/EigenValueSolver.cpp
@@ -1,5 +1,4 @@
 #include "EigenValueSolver.hpp"
-<<<<<<< HEAD
 #include <armadillo>
 #include <fstream>
 
@@ -17,25 +16,15 @@ EigenValueSolver::EigenValueSolver(double a, double d, int N){
     m_A(i,i-1) = a;
     m_A(i,i+1) = a;
   }
-=======
-
-
-void TridiagonalMatrixSolver::initialize(int N, double lambda)
-{
-  m_N = N;
-  double h = 1./(m_N+1); //Local variable, only needed in this function.
-  m_q = vec(m_N);
-  m_v = vec(m_N);
-  m_x = linspace(h, 1-h, m_N); //Only interior points of the mesh.
-  m_q = h*h*f(m_x);
 }
-
-void TridiagonalMatrixSolver::write_to_file(string filename)
-{
-  m_ofile.open(filename);
-  for (int i = 0; i < m_N; i++){
-    m_ofile << m_x(i) << " " << m_v(i) << endl;
+EigenValueSolver::EigenValueSolver(vec a, vec d){
+  m_N = d.n_rows;
+  m_A = mat(m_N,m_N,fill::zeros);
+  m_A(0,0) = d(0); m_A(0,1) = a(0);
+  m_A(m_N-1,m_N-1) = d(m_N-1); m_A(m_N-1,m_N-2) = a(m_N-2);
+  for (int i = 1; i < m_N - 1; i++){
+    m_A(i,i) = d(i);
+    m_A(i,i-1) = a(i-1);
+    m_A(i,i+1) = a(i);
   }
-  m_ofile.close();
->>>>>>> 0a97263b71751cd2bae799d7eb8de9e89da18de3
 }
