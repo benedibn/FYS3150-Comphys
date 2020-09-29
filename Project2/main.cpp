253fe00bd39edecf47e2ecd39b5001c698041493
torstbje <torstbje@student.matnat.uio.no>
2020-09-29 11:45:44 +0200

    File writing and debugging

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -9,29 +9,32 @@ using namespace std;
 using namespace arma;
 
 int main(int argc, char const *argv[]){
-  /*
-  double d = 2.;
-  double a = -1.;
-  int N = 5;
-  */
-  vec d(5,fill::ones);
-  vec a(4,fill::ones);
-  d *= 2; a *= (-1);
-  int N = d.n_rows;
+  (void) argc; (void) argv;
 
-  ArmaSolver aTriMat(a,d);
-  JacobiSolver aJacobi(a,d);
-  vec eigen(N);
-  vec eigen2(N);
-  eigen = aTriMat.solve();
-  for (int i = 0; i < N; i++){
-    cout << eigen(i) << endl;
+  int N = 6;
+  double a = -1;
+  double d = 2;
+
+  vec va(N-1,fill::ones);
+  va *= a;
+  vec vd(N,fill::ones);
+  vd *= d;
+
+  string fileName = "Eigenvalue.txt";
+  ofstream file;
+  file.open(fileName);
+
+
+  for (int i = 3; i < 10; i++){
+    JacobiSolver J(a,d,i);
+    J.solve();
+    J.writeToFile(file);
+    J.~JacobiSolver();
   }
-  cout << "------------------------------\n";
-  eigen2 = aJacobi.solve(100000);
-  for (int i = 0; i < N; i++){
-    cout << eigen2(i) << endl;
-  }
+
+
+  file.close();
+
 
   return 0;
 }
