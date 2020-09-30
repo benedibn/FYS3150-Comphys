d16f69c907a641bba588cfcb8fd5d916a3078107
trinekol <trikvam@gmail.com>
2020-09-30 15:41:53 +0200

    merge error gone

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -1,13 +1,8 @@
+
 #include "JacobiSolver.hpp"
 #include <fstream>
 #include <armadillo>
-<<<<<<< HEAD
-//#include "EigenValueSolver.cpp"
-//#include "JacobiSolver.cpp"
-//#include "ArmaSolver.cpp"
-=======
 #include "JacobiSolver.cpp"
->>>>>>> 962985443d67e7ce8c278a5ac3d4f747d4b2d1c9
 
 using namespace std;
 using namespace arma;
@@ -27,20 +22,9 @@ int main(int argc, char const *argv[]){
   string fileName = "Eigenvalue.txt";
   ofstream file;
   file.open(fileName);
-<<<<<<< HEAD
-
-  cout << "The numer of iterations the Jacobi method needed to make the off diagonal elements smaller than the tolerance" << endl;
-  cout << "dimension:    " << "iterations:" << endl;
-  for (int i = 3; i < 10; i++){
-    JacobiSolver J(a,d,i);
-    J.solve();
-    J.writeToFile(file);
-    J.~JacobiSolver();
-=======
   JacobiSolver J(a,d,N);
   if (J.unitTests()){
     cout << "it worked\n";
->>>>>>> 962985443d67e7ce8c278a5ac3d4f747d4b2d1c9
   }
   J.solve();
   J.writeToFile(file);
