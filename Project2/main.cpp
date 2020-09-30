3d6345dd713df83712de99b7ee8a4fa9d4422ba7
Benedicte Nyheim <benedibn@student.matnat.uio.no>
2020-09-30 19:45:40 +0200

    Made a more general initialize function that works for all tasks, b, d and e.

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -7,37 +7,36 @@
 using namespace std;
 using namespace arma;
 
+double V(double rho);
+
 int main(int argc, char const *argv[]){
   /*(void) argc;*/
 
   int N = atoi(argv[1]);
-  problem = string(argv[2]);
+  string problem = string(argv[2]);
 
-  double h = 1./(N+1);
-  double rho_0 = 0;
-  vec rho = rho(N-1,fill::ones);
-  for (int i = 0; i < N; i++){
-    rho[i] = rho_0 + i*h;
+  if (problem == "bucklingbeam"){
+
+    string fileName = problem + ".txt";
+    ofstream file;
+    file.open(fileName);
+    double a = 0;
+    double b = 1;
+    JacobiSolver J(a,b,N,V);
+    cout << "hallo" << endl;
+/*
+    if (J.unitTests(V)){
+      cout << "it worked\n";
+    }
+*/
+    J.solve();
+    J.writeToFile(file);
+    file.close();
   }
-
-  double a = -1./pow(h,2);
-  double d = 2./pow(h,2);
-
-
-  string fileName = problem + '.txt';
-  ofstream file;
-  file.open(fileName);
-
-  JacobiSolver J(a,d,N);
-
-  if (J.unitTests()){
-    cout << "it worked\n";
-  }
-
-  J.solve();
-  J.writeToFile(file);
-  file.close();
-
 
   return 0;
+}
+
+double V(double rho){
+  return 0.;
 }
--- Project2/main.py
+++ Project2/main.py
@@ -1,17 +1,9 @@
 import os
 import sys
 
-
-<<<<<<< HEAD
-os.system("c++ -o main.out *.cpp")
-
 N = int(input("Insert number of mesh point N: "))
-algorithm = input("Choose problem to solve: BucklingBeam/OneElectron/TwoElectrons: ")
-=======
+problem = input("Choose problem (bucklingbeam/oneelectron/twoelectrons): ")
 
-
-#N = int(input("Insert number of mesh point N: "))
-#problem = input("Choose problem (buckling beam/one electron/two electron): ")
 """
 filename_plot = "_".join([problem, "solution", str(N)]) + ".pdf" #Name of figure file
 filename_data = "_".join([problem, "N", str(N)]) + ".txt" #Name of data file.
@@ -26,7 +18,7 @@ os.system("g++ -o main.out main.cpp -larmadillo")
 
 #execute
 os.system("echo executing...")
-os.system("./main.out")
+os.system("./main.out" + " " + str(N) + " " + problem) #Execute code
 
 """
 #moves data files to result directory
@@ -45,4 +37,3 @@ os.system(" ".join(["mv", filename_plot, plot_path]))
 """
 #done
 os.system("echo done.")
->>>>>>> 2c6259b15792199e53aae28164cecfacfc0eb984
